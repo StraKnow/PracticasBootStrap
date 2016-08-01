c2050bff09f6a5ae36fa065711a235a25eaa7ccc
StraKnow <straknow@gmail.com>
2016-08-01 04:56:45 -0500

    Iniciando practicas de BootStrap

---
+++ app/scripts/app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name finalApp
+ * @description
+ * # finalApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('finalApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+
+      })
+      .when('/page2',{
+        templateUrl: 'views/page2.html',
+        
+      })
+
+      .when('/page3',{
+        templateUrl: 'views/page3.html',
+      })
+
+      .when('/acerca', {
+        templateUrl: 'views/acerca.html',
+      })
+
+      .when('/contacto', {
+        templateUrl: 'views/contacto.html',
+      })
+
+
+
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
